e923fd2200973e91baf3daba48c521a70fbb3c71
Michael Wales <mwales3@gmail.com>
2016-05-04 01:07:19 -0400

    GPIO testing on Raspberry Pi 3

--- GpioTester/GpioTester.cpp
+++ GpioTester/GpioTester.cpp
@@ -1,1 +1,29 @@
 // Add test code here
+#include <iostream>
+#include <unistd.h>
+#include "GpioIo.h"
+
+using namespace Arduino;
+
+int main(int argc, char** argv)
+{
+   std::cout << "hello world" << std::endl;
+
+   // Lets export GPIO 18
+   GpioIo dog(18, OUTPUT);
+
+   while(1)
+   {
+      //std::cout << "Turning on" << std::endl;
+      dog.setLevel(HIGH);
+
+      usleep(1000);
+
+      //std::cout << "Turning off" << std::endl;
+      dog.setLevel(LOW);
+
+      usleep(1000);
+
+   }
+
+}
